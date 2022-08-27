c32a78bf71ea31c68e60e4c06d5b1888d75674f3
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-08-27 18:47:52 +0530

    Show basic routing syntax

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
+import { BrowserRouter, Route } from "react-router-dom";
+
+import Welcome from "./pages/Welcome";
+import Products from "./pages/Products";
+
 function App() {
   return (
-    <div>
-      <h2>Let's get started!</h2>
-    </div>
+    <BrowserRouter>
+      <Route path="/welcome">
+        <Welcome />
+      </Route>
+      <Route path="/products">
+        <Products />
+      </Route>
+    </BrowserRouter>
   );
 }
 
-// '/' => component A
+// '/welcome' => component A
 // '/contact' => component B
 export default App;
