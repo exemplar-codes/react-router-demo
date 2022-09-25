a9a62fad7848436b61f6a9e12309b70f41aaef64
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-09-25 12:50:52 +0530

    Demo dynamic routes and accessing them

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { Route } from "react-router-dom";
+import { Route, useParams } from "react-router-dom";
+function MyComponent2() {
+  console.log(useParams(), MyComponent2.name);
+  return null;
+}
+
+function MyComponent() {
+  console.log(useParams(), MyComponent.name);
+  return <MyComponent2 />;
+}
 
 function App() {
   return (
     <div>
-      <Route path="/products">
+      <Route path="/" exact>
         <h1>Products page</h1>
       </Route>
-      <Route path="/products/:id">Specific Product</Route>
+      <Route path="/:dParam">
+        <MyComponent />
+        Specific Product
+      </Route>
     </div>
   );
 }
