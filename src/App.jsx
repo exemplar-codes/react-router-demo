0fc5a0b58d786de7ecd404bf054ad7a7aae3ddcc
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-09-25 11:57:29 +0530

    Demo redirect

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,12 @@
-import { Route } from "react-router-dom";
+import { Redirect, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
-      <Route path="/alice">
-        <h1>Alice</h1>
-        <Route path="/alice/bob">
-          {/* <!-- will be rendered on URL /alice/bob --> */}
-          <h1>Alice/Bob</h1>
-        </Route>
+      <Route path="/" exact>
+        <Redirect to="/login" />
       </Route>
+      <Route path="/login">Login page</Route>
     </div>
   );
 }
