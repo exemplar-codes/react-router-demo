bad3933dc40fff6bd004fa91640d8b8eb41be49e
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-09-27 09:05:38 +0530

    Demo programmatic navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
-import { Link, Prompt } from "react-router-dom";
+import { Route, useHistory } from "react-router-dom";
 
 function App() {
+  const history = useHistory();
+
+  const goToWelcome = () => history.push("welcome");
+  const goHome = () => history.push("/");
+
   return (
     <>
-      <Link to="welcome">Click</Link>
-      <Prompt when={true} message={(location) => "Leave page?"} />
+      <Route path="/" exact>
+        <button onClick={goToWelcome}>Go to Welcome</button>
+      </Route>
+      <Route path="/welcome">
+        <h1>Welcome Page</h1>
+        <button onClick={goHome}>Go Home</button>
+      </Route>
     </>
   );
 }
