cdce6584b05592b26047d352319b35cd7629efa3
Muhammad Sanjar Afaq <sanjarcode@gmail.com>
2022-10-01 18:20:39 +0530

    Add timed example

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,27 @@
+import { useEffect, useState } from "react";
 import { Link, Prompt, Route } from "react-router-dom";
 
 function App() {
+  const [prompt, setPrompt] = useState(true);
+
+  const time = 5000;
+  useEffect(() => {
+    setTimeout(() => setPrompt(false), time);
+  }, []);
+
   return (
     <>
       <Route path="/" exact>
         <h1>Home page</h1>
         <Link to="/welcome">Click</Link>
-        <Prompt when={true} message={(location) => "Leave page?"} />
+        <br />
+        <Prompt when={prompt} message={(location) => "Leave page?"} />
+        {prompt
+          ? `Working... ETA: ${time / 1000} seconds`
+          : "You can leave the page"}
       </Route>
       <Route path="/welcome">
-        <h1>Welcome</h1>
+        <h1>Welcome page</h1>
       </Route>
     </>
   );
